5e7eac1d70fda9a07b1ce4c1da2a76439a5834d3
NaotakaSaito <saitou088@dsn.lapis-semi.com>
2017-12-23 01:36:00 +0000

    update 171223

---
+++ ubuntu-setup.sh
@@ -0,0 +1,12 @@
+# setup 
+sudo apt-get install -y vim apache2
+sudo apt-get install -y build-essential
+sudo apt-get install sqlite3
+# installing node.js ver6.x
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm node-gyp
+
+#install for S3
+sudo apt-get install automake autotools-dev g++ git libcurl4-gnutls-dev libfuse-dev libssl-dev libxml2-dev make pkg-config
+
